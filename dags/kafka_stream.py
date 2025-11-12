062d7b947af0148e870a9726825371c6ef06dba2
Ash-bot19 <Ayushsam3@gmail.com>
2025-11-13 01:12:17 +0530

    WIP: current state before branching

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,51 +1,65 @@
 from datetime import datetime
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-
-default_args = {
-    'owner': 'airflow',
-    'start_date': datetime(2023, 9, 3, 10, 00), 
-}
+from airflow.operators.python_operator import PythonOperator
 
 def get_data():
     import requests
-
-    res = requests.get("https://randomuser.me/api/")
-    res = res.json()
-    res = res['results'][0]
-   
-    return res
+    r = requests.get("https://randomuser.me/api/", timeout=15)
+    r.raise_for_status()
+    return r.json()['results'][0]
 
 def format_data(res):
-    data = {}
-    location = res['location']
-    data['first_name'] = res['name']['first']
-    data['last_name'] = res['name']['last']
-    data['gender'] = res['gender']
-    data['address'] = f"{location['street']['number']} {location['street']['name']}, {location['city']}, {location['state']}, {location['country']}, {location['postcode']}"
-    data['postal_code'] = location['postcode']
-    data['email'] = res['email']
-    data['username'] = res['login']['username']
-    data['date_of_birth'] = res['dob']['date']
-    data['registered_date'] = res['registered']['date']
-    data['phone'] = res['phone']
-    data['picture'] = res['picture']['medium']
-    return data
+    loc = res['location']
+    return {
+        'first_name': res['name']['first'],
+        'last_name': res['name']['last'],
+        'gender': res['gender'],
+        'address': f"{loc['street']['number']} {loc['street']['name']}, "
+                   f"{loc['city']}, {loc['state']}, {loc['country']} {loc['postcode']}",
+        'email': res['email'],
+        'username': res['login']['username'],
+        'dob': res['dob']['date'],
+        'registered': res['registered']['date'],
+        'phone': res['phone'],
+        'picture': res['picture']['medium'],
+    }
 
-def stream_data():
-    import  json
-    res = get_data()
-    res = format_data(res)
-    print(json.dumps(res, indent=3))
+def stream_to_kafka():
+    import json, logging, time
+    from kafka import KafkaProducer
 
-""" with DAG('user_automation_dag', 
-         default_args=default_args, 
-         schedule_interval='@daily', 
-         catchup=False) as dag:
-
-    streaming_task = PythonOperator(
-        task_id='streaming_data_from_api',
-        python_callable=stream_data_from_api,
+    producer = KafkaProducer(
+        bootstrap_servers=['broker:29092'],
+        value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+        retries=3,
+        linger_ms=50,
+        request_timeout_ms=10000,
+        max_block_ms=10000,
     )
- """
-stream_data();
+
+    for _ in range(5):
+        try:
+            res = get_data()
+            data = format_data(res)
+            producer.send('users_created', data)
+            logging.info("Sent user %s", data['username'])
+            time.sleep(1)
+        except Exception as e:
+            import traceback
+            logging.error(traceback.format_exc())
+            raise
+
+
+default_args = {'owner': 'airflow', 'start_date': datetime(2023, 9, 3, 10, 0)}
+
+with DAG(
+    dag_id='user_automation_dag',
+    default_args=default_args,
+    schedule_interval=None,
+    catchup=False,
+) as dag:
+    stream_task = PythonOperator(
+        task_id='stream_data_from_api',
+        python_callable=stream_to_kafka,
+    )
+
